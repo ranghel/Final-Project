12b41fc1d5ea910d07612168d07792b64fe12fbe
ranghel <anghelramona30@gmail.com>
2017-07-09 01:59:46 -0400

    still deployment issues

--- server.js
+++ server.js
@@ -1,33 +1,36 @@
 var express = require('express');
 var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
-var app = express();
-
-
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 8000;
 
-//static content
+// Requiring our models for syncing
+var db = require("./models");
+
+// Static directory
 app.use(express.static(process.cwd() + '/public'));
 
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({extended: false}));
 
 
-//override with POST command
+// Method Override
 app.use(methodOverride('_method'));
 
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-
+//Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 var routes = require('./controllers/helpinghands_controller.js');
 app.use('/', routes);
 
-
-app.listen(process.env.PORT || 8000);
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT: " + PORT);
+    });
+});
 
